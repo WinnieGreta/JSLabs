13eb33249d640f2eb4125b6d024bf98627b3e0d7
WinnieGreta <trushchakolga@gmail.com>
2024-12-13 17:46:01 -0500

    fix product display

--- ProductSurvey/product_survey.js
+++ ProductSurvey/product_survey.js
@@ -15,7 +15,8 @@ function submitFeedback() {
     document.getElementById('userEmail').innerHTML = email;
     document.getElementById('userJob').innerHTML = job;
     document.getElementById('userDesignation').innerHTML = designation;
-    document.getElementById('userProductChoice').innerHTML = productType;
+    document.getElementById('userProductChoice1').innerHTML = productType;
+    document.getElementById('userProductChoice2').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
     document.getElementById('userExperience').innerHTML = experience;
 
