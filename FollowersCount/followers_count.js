ba1e239c704c2dcdc81c19cb637c340b51d77033
WinnieGreta <trushchakolga@gmail.com>
2024-12-13 17:03:23 -0500

    add reset functionality to interactive counter

--- FollowersCount/followers_count.js
+++ FollowersCount/followers_count.js
@@ -15,5 +15,13 @@ function checkCountValue() {
         alert("10 followers, congrats!");
     } else if (count === 20) {
         alert("20 followers, keep it up!");
+    } else if (count === 0) {
+        alert("count reset");
     }
+}
+
+function reset() {
+    count = 0;
+    displayCount();
+    checkCountValue();
 }
